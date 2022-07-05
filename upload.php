ad5109d9378b5b5eb2ed75fa95d347225c2adff6
fcasfs-of <94762834+fcasfs-of@users.noreply.github.com>
2022-07-04 22:59:34 -0300

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,27 +1,36 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Upload your files</title>
-</head>
-<body>
-  <form enctype="multipart/form-data" action="upload.php" method="POST">
-    <p>Upload your file</p>
-    <input type="file" name="uploaded_file"></input><br />
-    <input type="submit" value="Upload"></input>
-  </form>
-</body>
-</html>
-<?PHP
-  if(!empty($_FILES['uploaded_file']))
-  {
-    $path = "uploads/";
-    $path = $path . basename( $_FILES['uploaded_file']['name']);
+<?php 
 
-    if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $path)) {
-      echo "The file ".  basename( $_FILES['uploaded_file']['name']). 
-      " has been uploaded";
-    } else{
-        echo "There was an error uploading the file, please try again!";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_FILES['files'])) {
+        $errors = [];
+        $path = 'uploads/';
+	$extensions = ['txt_init', 'b64txt_init', 'b64txt_init'];
+		
+        $all_files = count($_FILES['files']['tmp_name']);
+
+        for ($i = 0; $i < $all_files; $i++) {  
+		$file_name = $_FILES['files']['name'][$i];
+		$file_tmp = $_FILES['files']['tmp_name'][$i];
+		$file_type = $_FILES['files']['type'][$i];
+		$file_size = $_FILES['files']['size'][$i];
+		$tmp = explode('.', $_FILES['files']['name'][$i]);
+		$file_ext = strtolower(end($tmp));
+
+		$file = $path . $file_name;
+
+		if (!in_array($file_ext, $extensions)) {
+			$errors[] = 'Extension not allowed: ' . $file_name . ' ' . $file_type;
+		}
+
+		if ($file_size > 2097152) {
+			$errors[] = 'File size exceeds limit: ' . $file_name . ' ' . $file_type;
+		}
+
+		if (empty($errors)) {
+			move_uploaded_file($file_tmp, $file);
+		}
+	}
+
+	if ($errors) print_r($errors);
     }
-  }
-?>
+}
